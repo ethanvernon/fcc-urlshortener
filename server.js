36bf73abf0acb151027fc6b097b714754dbcdac8
Ethan Vernon <ethan.vernon@colorado.edu>
2019-03-12 20:12:57 +0800

    check if url already in database and returns if so

--- server.js
+++ server.js
@@ -50,26 +50,64 @@ MongoClient.connect(process.env.MONGO_URI, function(err, db) {
 			//get the url from the form
 			var newUrl=req.body.url;
 
-			//make the object to store
-			var urlToShorten = new ShortUrl({
-				original_url: newUrl, 
-				short_url:1
-			});
-
 			//open the collection from db atlas
 			var collection=db.db("FCC").collection("links");
 
-			//save the new object
-			collection.save(urlToShorten, err => {
-				if (err) {
-					console.log("error to databse: " + err);
+			//check if the url is in the right format
+
+
+			//check if the url is in the database already and return the shortened one if so
+			var findExistingEntry = collection.findOne(
+			    { original_url: newUrl },
+			    { original_url: 1, short_url: 1}
+			).then(function(data) {
+				if (data) {
+					console.log('already here');
+					console.log(data);
+					console.log(data.short_url);
+					return res.send({original_url: data.original_url, short_url: data.short_url});
+				} else {
+					//make the object to store
+					var urlToShorten = new ShortUrl({
+						original_url: newUrl, 
+						short_url:1
+					});
+
+					//save the new object
+					collection.save(urlToShorten, err => {
+						if (err) {
+							console.log("error to databse: " + err);
+						}
+					});
+
+					//show new object in browser
+					return res.send({original_url:urlToShorten.original_url, short_url:urlToShorten.short_url});
 				}
 			});
 
-			console.log(urlToShorten);
+			/*if (findExistingEntry) {
+				console.log('already here');
+				console.log(findExistingEntry);
+				console.log(findExistingEntry.short_url);
+			} else {
+				//make the object to store
+				var urlToShorten = new ShortUrl({
+					original_url: newUrl, 
+					short_url:1
+				});
 
-			//show new object
-			return res.send({original_url:urlToShorten.original_url, short_url:urlToShorten.short_url});
+				//save the new object
+				collection.save(urlToShorten, err => {
+					if (err) {
+						console.log("error to databse: " + err);
+					}
+				});
+
+				//show new object in browser
+				return res.send({original_url:urlToShorten.original_url, short_url:urlToShorten.short_url});
+			}*/
+
+
 			
 		});
 

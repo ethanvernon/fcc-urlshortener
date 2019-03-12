3a530c39a042209cc32bb10017fe9786ff575ef3
Ethan Vernon <ethan.vernon@colorado.edu>
2019-03-12 15:14:53 +0800

    heroku first commit

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var port = process.env.PORT || 3000;
 /** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 
+
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
@@ -29,11 +30,7 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-//get info from post request
-app.post('/api/shorturl/new', function(req,res) {
-	var newUrl=req.body.url;
-	res.json({"test": newUrl});
-});
+
 
 //make schema/model for a shortened url
 var Schema = mongoose.Schema;
@@ -42,14 +39,37 @@ var urlSchema = new Schema({
 	original_url: {
 		type:String,
 		unique:true
-		}
+		},
 	short_url: {
 		type:Number,
 		unique:true
 	}
 });
 
-var shortUrl = mongoose.model('shortUrl', urlSchema);
+var ShortUrl = mongoose.model('ShortUrl', urlSchema);
+
+
+//create a URL
+var createShortUrl = function(done, postUrl) {
+	console.log('doc create');
+	var ToShortUrl = new ShortUrl({original_url: postUrl, short_url:1});
+	ToShortUrl.save((err, data) => err ? done(err) : done(null, data));
+};
+
+//get info from post request
+app.post('/api/shorturl/new', function(req,res) {
+	var newUrl=req.body.url;
+	console.log(newUrl);
+	console.log(createShortUrl);
+	createShortUrl(null, newUrl);
+	res.json({"test": newUrl});
+});
+
+//how to find a url in database
+var findOneByOriginal = function(original, done) {
+  var searchOriginals = {original_url:original};
+  ShortUrl.findOne(searchOriginals, (err, data) => err ? done(err) : done(null, data));
+};
 
 //console for port
 app.listen(port, function () {

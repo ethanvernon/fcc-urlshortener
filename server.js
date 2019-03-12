677c3798a25717f0800ecc71506d086ce33f3daa
Ethan Vernon <ethan.vernon@colorado.edu>
2019-03-12 15:20:36 +0800

    updates port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cors = require('cors');
 var app = express();
 
 // Basic Configuration 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
 
 /** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
